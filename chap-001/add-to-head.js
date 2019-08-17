0dffd14423d529d316bee4def41b884a642eb537
Dulani Wallace <dulani@dulani.com>
2019-08-17 01:02:55 -0400

    Adds new method to the linked list, establishes newNode, becomes head and tail

--- chap-001/add-to-head.js
+++ chap-001/add-to-head.js
@@ -10,6 +10,14 @@ function Node(value, next, prev) {
 }
 
 /* adds new node to head */
-LinkedList.prototype.addToHead = function(value) {
+
+LinkedList.prototype.addToHead = function(value) { 
     var newNode = new Node(value, this.head, null);
+    if (this.head) this.head.prev = newNode;
+    else this.tail = newNode;
+    this.head = newNode;  
 };
+
+/* adds a new method to the LinkedList */
+/* 14 a new method called addToHead */
+/* 15 a new node with node constructor function & assigned it to the name new Node*/
