331f2a95480d2a014219700408b130fb13a6d405
Dulani Wallace <dulani@dulani.com>
2019-08-17 02:13:01 -0400

    Adds variable and nodes

---
+++ chap-001/test-head-tail.js
@@ -0,0 +1,37 @@
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) { 
+    var newNode = new Node(value, this.head, null);
+    if (this.head) this.head.prev = newNode;
+    else this.tail = newNode;
+    this.head = newNode;  
+};
+
+LinkedList.prototype.addToTail = function(value) {
+    var newNode = new Node(value, null, this.tail);
+    if (this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
+};
+
+var myLL = new LinkedList();
+
+myLL.addToTail(10);
+myLL.addToTail(20);
+myLL.addToTail(30);
+myLL.addToHead(100);
+
+// console.log(myLL.tail.prev);
+// console.log(myLL.tail.prev.prev);
+// console.log(myLL);
+
+console.log(myLL.tail.prev.prev.prev);
