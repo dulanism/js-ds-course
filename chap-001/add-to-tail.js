e9ea3a093782d4fadeb7f5ce1fe3688deed9c63e
Dulani Wallace <dulani@dulani.com>
2019-08-17 01:49:15 -0400

    Add new add to tail method

---
+++ chap-001/add-to-tail.js
@@ -0,0 +1,24 @@
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) { 
+    var newNode = new Node(value, this.head, null);
+    if (this.head) this.head.prev = newNode;
+    else this.tail = newNode;
+    this.head = newNode;  
+};
+
+LinkedList.prototype.addToTail = function(value) {
+    var newNode = new Node(value, null this.tail);
+    if (this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
+};
