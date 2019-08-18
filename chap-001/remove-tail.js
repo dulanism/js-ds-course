fe36514384036dee9cecab56bc33c577b3038024
Dulani Wallace <dulani@dulani.com>
2019-08-18 01:19:14 -0400

    Function to remove tail added

---
+++ chap-001/remove-tail.js
@@ -0,0 +1,49 @@
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+// Creating Head Nodes
+LinkedList.prototype.addToHead = function(value) { 
+    var newNode = new Node(value, this.head, null);
+    if (this.head) this.head.prev = newNode;
+    else this.tail = newNode;
+    this.head = newNode;  
+};
+
+// Creating Tail Nodes
+LinkedList.prototype.addToTail = function(value) {
+    var newNode = new Node(value, null, this.tail);
+    if (this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
+};
+
+// if the list is empty
+// LinkedList.prototype.removeHead = function() {
+//    if(!this.head) return null;
+//};
+
+// function to remove tail
+LinkedList.prototype.removeTail = function() {
+    if (!this.tail) return null;
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+    if (this.tail) this.tail.next = null;
+    else this.head = null;
+    return val;
+};
+
+var ll = new LinkedList();
+
+ll.addToHead('one');
+ll.addToHead('two');
+ll.addToHead('three');
+
+console.log(ll.removeTail());
