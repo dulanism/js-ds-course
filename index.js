c314add4869e7a44d075155917309393c094438b
Dulani Wallace <dulani@dulani.com>
2020-02-08 19:55:03 -0500

    modified js file with template strings

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let lname = 'D';
 let age = prompt("Guess Ryan's age..");
 
 
-//old way
-let result = fname + ' ' + lname + ' ' + age + ' years old';
+// old way
+// let result = fname + ' ' + lname + ' ' + age + ' years old';
+// alert(result);
+
+// using template strings
+let result = `${fname} ${lname} is ${age} years old`;
 alert(result);
